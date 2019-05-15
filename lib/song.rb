eeac9aa15a340967793e637202a4edd19e692fbf
Kevin Dai <daikevin19@yahoo.com>
2019-05-15 20:10:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @@count += 1
+    @@genres << self.genre
+    @@artists << self.artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    @@artists
+  end
+end
